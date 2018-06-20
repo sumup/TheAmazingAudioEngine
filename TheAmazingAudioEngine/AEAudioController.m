4a723dd950b30b91b64ff0ca2363db5d97b1b588
Sebastian Hagedorn <sebastian.hagedorn@sumup.com>
2018-06-20 17:20:43 +0200

    Merge upstream master
    * commit 'd9990a9c0d8ebccc76e89a9b9678265614e57570':
      Only set A2DP flag for PlayAndRecord audio session

--- TheAmazingAudioEngine/AEAudioController.m
+++ TheAmazingAudioEngine/AEAudioController.m
@@ -1959,8 +1959,8 @@ BOOL AECurrentThreadIsAudioThread(void) {
 #if !TARGET_OS_TV
     if ( [_audioSessionCategory isEqualToString:AVAudioSessionCategoryPlayAndRecord] ) {
         options |= AVAudioSessionCategoryOptionDefaultToSpeaker;
+        options |= _enableBluetoothInput ? AVAudioSessionCategoryOptionAllowBluetoothA2DP : 0;
     }
-    options |= _enableBluetoothInput ? AVAudioSessionCategoryOptionAllowBluetooth : 0;
 #endif
     
     if ( [_audioSessionCategory isEqualToString:AVAudioSessionCategoryPlayAndRecord] || [_audioSessionCategory isEqualToString:AVAudioSessionCategoryPlayback] ) {
